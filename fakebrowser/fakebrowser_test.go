177a26ba0d025093b0b25ef72331265f80c61c72
Will McCutchen <will@mccutch.org>
2021-05-01 14:16:08 -0400

    Drop everything but the core functionality (#13)

--- fakebrowser/fakebrowser_test.go
+++ fakebrowser/fakebrowser_test.go
@@ -1,5 +1,5 @@
 //nolint:errcheck
-package urlresolver
+package fakebrowser
 
 import (
 	"net/http"
@@ -9,45 +9,61 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func addHeaders(t *testing.T, a, b map[string]string) map[string]string {
+func mergeMaps(t *testing.T, maps ...map[string]string) map[string]string {
 	result := make(map[string]string)
-	for k, v := range a {
-		result[k] = v
-	}
-	for k, v := range b {
-		result[k] = v
+	for _, m := range maps {
+		for k, v := range m {
+			result[k] = v
+		}
 	}
 	return result
 }
 
-func TestFakeBrowserTransport(t *testing.T) {
+func TestHeaderInjection(t *testing.T) {
 	t.Parallel()
 
 	testCases := map[string]struct {
-		injectHeaders  map[string]string
+		transport      *Transport
 		requestHeaders map[string]string
 		wantHeaders    map[string]string
 	}{
 		"headers are injected": {
+			transport: New(http.DefaultTransport),
 			requestHeaders: map[string]string{
 				"X-1": "in request",
 				"X-2": "in request",
 			},
-			wantHeaders: addHeaders(t, fakeBrowserHeaders, map[string]string{
+			wantHeaders: mergeMaps(t, DefaultHeaders, map[string]string{
 				"Accept-Encoding": "gzip", // added by stdlib http client
 				"X-1":             "in request",
 				"X-2":             "in request",
 			}),
 		},
-		"existing headers take precedence": {
+		"existing headers override injected headers": {
+			transport: New(http.DefaultTransport),
 			requestHeaders: map[string]string{
 				"User-Agent": "in request",
 				"X-1":        "in request",
 				"X-2":        "in request",
 			},
-			wantHeaders: addHeaders(t, fakeBrowserHeaders, map[string]string{
+			wantHeaders: mergeMaps(t, DefaultHeaders, map[string]string{
+				"Accept-Encoding": "gzip",       // added by stdlib http client
+				"User-Agent":      "in request", // will override value in DefaultHeaders
+				"X-1":             "in request",
+				"X-2":             "in request",
+			}),
+		},
+		"injected headers can be customized": {
+			transport: New(http.DefaultTransport, WithHeaders(map[string]string{
+				"User-Agent": "custom-user-agent",
+			})),
+			requestHeaders: map[string]string{
+				"X-1": "in request",
+				"X-2": "in request",
+			},
+			wantHeaders: mergeMaps(t, map[string]string{
 				"Accept-Encoding": "gzip", // added by stdlib http client
-				"User-Agent":      "in request",
+				"User-Agent":      "custom-user-agent",
 				"X-1":             "in request",
 				"X-2":             "in request",
 			}),
@@ -74,11 +90,7 @@ func TestFakeBrowserTransport(t *testing.T) {
 				req.Header.Set(k, v)
 			}
 
-			client := &http.Client{
-				Transport: &fakeBrowserTransport{
-					transport: http.DefaultTransport,
-				},
-			}
+			client := &http.Client{Transport: tc.transport}
 			resp, err := client.Do(req)
 			assert.NoError(t, err)
 			assert.Equal(t, resp.StatusCode, http.StatusOK)
--- go.mod
+++ go.mod
@@ -5,16 +5,11 @@ go 1.16
 require (
 	github.com/PuerkitoBio/purell v1.1.1
 	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
-	github.com/alicebob/miniredis/v2 v2.14.3
-	github.com/go-redis/cache/v8 v8.4.0
-	github.com/go-redis/redis/v8 v8.8.2
-	github.com/honeycombio/beeline-go v1.0.0
-	github.com/rs/zerolog v1.21.0
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/pretty v0.2.0 // indirect
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	golang.org/x/text v0.3.6
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
-
-// https://github.com/honeycombio/beeline-go/pull/216
-replace github.com/honeycombio/beeline-go v1.0.0 => github.com/mccutchen/beeline-go v1.0.1
